5131bbb364b0ccaa4bd0eeebe526054fb19126ac
aryapg <110161253+aryapg@users.noreply.github.com>
2023-10-06 15:57:26 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,16 +1,65 @@
-# Recursive function to calculate factorial
+# Function to calculate the factorial of a non-negative integer
 def factorial(n):
+    """
+    Calculate the factorial of a non-negative integer.
+
+    Args:
+        n (int): The non-negative integer for which to calculate the factorial.
+
+    Returns:
+        int: The factorial of the input integer.
+    """
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
     if n == 0:
         return 1
     else:
-        return n * factorial(n - 1)
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
 
-# Input a number from the user
-num=2
+# Test cases for the factorial function
+def test_factorial_positive():
+    """
+    Test positive integer inputs for the factorial function.
+    """
+    # Test the factorial of 0
+    assert factorial(0) == 1
 
-# Check if the input is non-negative
-if num < 0:
-    print("Factorial is not defined for negative numbers.")
-else:
-    result = factorial(num)
-    print(f"The factorial of {num} is {result}")
+    # Test the factorial of 1
+    assert factorial(1) == 1
+
+    # Test the factorial of 2: 2! = 2
+    assert factorial(2) == 2
+
+    # Test the factorial of 5: 5! = 120
+    assert factorial(5) == 120
+
+    # Test the factorial of 10: 10! = 3,628,800
+    assert factorial(10) == 3628800
+
+    # Test the factorial of 20: 20! = 2,432,902,008,176,640,000
+    assert factorial(20) == 2432902008176640000
+
+def test_factorial_negative():
+    """
+    Test that the factorial function raises a ValueError for negative inputs.
+    """
+    try:
+        # Attempt to calculate the factorial of -1, which should raise a ValueError
+        factorial(-1)
+    except ValueError as e:
+        # Check that the error message matches the expected message
+        assert str(e) == "Factorial is not defined for negative numbers."
+
+def test_factorial_large_input():
+    """
+    Test the factorial of a large input value.
+    """
+    # Test the factorial of 50 (a large number)
+    assert factorial(50) == 30414093201713378043612608166064768844377641568960512000000000000
+
+if __name__ == "__main__":
+    import pytest
+    pytest.main()
